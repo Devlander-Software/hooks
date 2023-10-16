a95d0ddd3a75bee3ce831b734a59235718f42b0c
Landon Johnson <landonwjohnson@gmail.com>
2023-10-16 14:37:15 -0600

    updated build

--- refresh-modules.js
+++ refresh-modules.js
@@ -1,34 +1,65 @@
-const fs = require("fs");
-const { execSync } = require("child_process");
+// File: refresh-modules.js (JavaScript)
 
-const executeCommand = (command) => {
+const fs = require('fs');
+const { execSync } = require('child_process');
+const { syncPackageVersions } = require(__dirname + '/external-modules/sync-dep-versions'); // Use __dirname
+const chalk = require('chalk'); // Import chalk
+
+/**
+ * Execute a command and handle errors.
+ *
+ * @param {string} command - The command to execute.
+ */
+function executeCommand(command) {
     try {
         execSync(command);
     } catch (error) {
-        console.error(`Error executing command "${command}":`, error.stdout?.toString());
+        console.error(chalk.red(`Error executing command "${command}":`), error.stdout?.toString());
         console.error(error.stderr?.toString());
         // Depending on your use case, you might want to rethrow the error or exit
-        // process.exit(1); 
+        // process.exit(1);
     }
-};
+}
 
-const requireModule = (modulePath) => {
+/**
+ * Require a module and handle errors.
+ *
+ * @param {string} modulePath - The path to the module to require.
+ */
+function requireModule(modulePath) {
     try {
         require(modulePath);
     } catch (error) {
-        console.error(`Error requiring module "${modulePath}":`, error.message);
+        console.error(chalk.red(`Error requiring module "${modulePath}":`), error.message);
         // Depending on your use case, you might want to rethrow the error or exit
-        // process.exit(1); 
-    }
-};
-
-if (fs.existsSync("./external-modules")) {
-
-    if (fs.existsSync("./external-modules/yarn-upgrade-modules")) {
-        requireModule("./external-modules/yarn-upgrade-modules");
-    }
-
-    if (fs.existsSync("./external-modules/sync-dep-versions")) {
-        executeCommand("node ./external-modules/sync-dep-versions/index.js @devlander/react-native-shared-types ./package.json");
+        // process.exit(1);
     }
 }
+
+/**
+ * Main function to synchronize dependencies and execute scripts.
+ */
+function main() {
+    if (!fs.existsSync(__dirname + '/external-modules')) { // Use __dirname
+        console.error(chalk.red('The "external-modules" directory does not exist.'));
+        return;
+    }
+
+    // Refresh modules using syncPackageVersions
+    syncPackageVersions('@devlander/shared-react-native-types', './package.json');
+
+    const yarnUpgradeModulesPath = __dirname + '/external-modules/yarn-upgrade-modules.js'; // Use __dirname
+    const syncDepVersionsIndexPath = __dirname + '/external-modules/sync-dep-versions/index.js'; // Use __dirname
+
+    // Require and execute yarn-upgrade-modules.js if it exists
+    if (fs.existsSync(yarnUpgradeModulesPath)) {
+        requireModule(yarnUpgradeModulesPath);
+    }
+
+    // Execute sync-dep-versions/index.js if it exists
+    if (fs.existsSync(syncDepVersionsIndexPath)) {
+        executeCommand(`node ${syncDepVersionsIndexPath} @devlander/react-native-shared-types ./package.json`);
+    }
+}
+
+main();
