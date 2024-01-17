8d39e70db9307efdcf80729c74d5b68901da0d57
landonwjohnson <landonwjohnson@gmail.com>
2024-01-17 13:54:01 -0500

    updated useScreenDimensions hook so you can pass options into it and get the window or height

--- src/hooks/shared/useScreenDimensions/useScreenDimensions.test.tsx
+++ src/hooks/shared/useScreenDimensions/useScreenDimensions.test.tsx
@@ -1,5 +1,8 @@
 import { renderHook } from "@testing-library/react-hooks"
-import { useScreenDimensions } from "./useScreenDimensions.hook"; // Import your hook
+import {
+  useScreenDimensions,
+  UseScreenOptions,
+} from "./useScreenDimensions.hook" // Import your hook
 
 // Mock Dimensions module for React Native Web
 jest.mock("react-native-web", () => ({
@@ -11,7 +14,11 @@ jest.mock("react-native-web", () => ({
 }))
 
 describe("useScreenDimensions", () => {
-  it("returns the screen dimensions", () => {
+  afterEach(() => {
+    jest.restoreAllMocks() // Cleanup after each test
+  })
+
+  it("returns the screen dimensions with default options", () => {
     const screenDimensions = { width: 320, height: 480 }
 
     // Mock Dimensions.get to return the screen dimensions
@@ -19,13 +26,42 @@ describe("useScreenDimensions", () => {
       .spyOn(global as any, "Dimensions", "get")
       .mockReturnValue(screenDimensions)
 
-    // Render the hook
+    // Render the hook with default options
     const { result } = renderHook(() => useScreenDimensions())
 
     // Assert that the hook returns the correct screen dimensions
     expect(result.current).toEqual(screenDimensions)
+  })
 
-    // Cleanup
-    jest.restoreAllMocks()
+  it("returns the screen dimensions with 'window' scaleType", () => {
+    const screenDimensions = { width: 480, height: 800 }
+    const options: UseScreenOptions = { scaleType: "window" }
+
+    // Mock Dimensions.get to return the screen dimensions
+    jest
+      .spyOn(global as any, "Dimensions", "get")
+      .mockReturnValue(screenDimensions)
+
+    // Render the hook with 'window' scaleType
+    const { result } = renderHook(() => useScreenDimensions(options))
+
+    // Assert that the hook returns the correct screen dimensions
+    expect(result.current).toEqual(screenDimensions)
+  })
+
+  it("returns the screen dimensions with 'screen' scaleType", () => {
+    const screenDimensions = { width: 1920, height: 1080 }
+    const options: UseScreenOptions = { scaleType: "screen" }
+
+    // Mock Dimensions.get to return the screen dimensions
+    jest
+      .spyOn(global as any, "Dimensions", "get")
+      .mockReturnValue(screenDimensions)
+
+    // Render the hook with 'screen' scaleType
+    const { result } = renderHook(() => useScreenDimensions(options))
+
+    // Assert that the hook returns the correct screen dimensions
+    expect(result.current).toEqual(screenDimensions)
   })
 })
